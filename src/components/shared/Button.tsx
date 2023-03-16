52a624ae6f09044405fddbed7384a26e0d675188
Mansoor Akhter <mansoor21st@Gmail.com>
2023-03-16 16:39:58 +0500

    button and hero section responsive

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const Button: FC<{ title: string }> = ({ title }) => {
     return (
         <button type="button"
-            className="bg-[#00616C] py-3 px-6 rounded-full text-white text-lg font-medium hover:scale-105 duration-300 hover:shadow-lg">
+            className="bg-[#00616C] py-2 px-4 text-sm sm:text-lg sm:py-3 sm:px-6 rounded-full text-white font-medium hover:scale-105 duration-300 hover:shadow-lg">
             {title}
         </button>);
 };
--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -10,17 +10,17 @@ const Hero = () => {
             <Wrapper>
                 <div className="flex flex-col md:flex-row items-center">
                     <div className="flex-1 leading-normal">
-                        <h4 className="text-[#00616C] font-semibold mt-4">
+                        <h4 className="text-[#00616C] font-semibold mt-8 sm:mt-4">
                             Presidential Initiative for Artificial Intelligence and Computing (PIAIC)
                         </h4>
-                        <h1 className="text-4xl font-extrabold sm:text-6xl my-3">
+                        <h1 className="text-4xl font-extrabold sm:text-6xl my-6">
                             Certified Web 3.0 and Metaverse Developer
                         </h1>
 
                         <p className="text-slate-700 max-w-xl">
                             A One and Quarter Years Panaverse DAO Earn as you Learn Program Getting Ready for the Next Generation of the Internet
                         </p>
-                        <p className="text-slate-700 my-3">
+                        <p className="text-slate-700 my-6">
                             Consolidating Web 3.0, Metaverse, Artificial Intelligence (AI), Cloud, Edge, Ambient Computing/IoT, Network Automation, and Bioinformatics Technologies
                         </p>
                         <div className="mt-6">
@@ -29,7 +29,7 @@ const Hero = () => {
 
                     </div>
 
-                    <div className="flex flex-1 justify-end items-center">
+                    <div className="flex flex-1 justify-end items-center mt-8 sm:mt-0">
                         <Image src={heroPoster} alt="Hero Poster" className="md:max-w-xl" style={{ width: "100%", height: "auto" }} />
                     </div>
                 </div>
